417d7a7494b23719063c5bff50020e3f4c0dc927
Scripsi <scripsi@scripsi.uk>
2019-06-09 15:02:16 +0100

    Updated encoder to add push button and event queue

--- encoder.py
+++ encoder.py
@@ -1,17 +1,27 @@
 #!/usr/bin/python
 
 from gpiozero import Button
+import Queue
 
-pin_a = Button(5,pull_up=True)         # Rotary encoder pin A connected to GPIO5
-pin_b = Button(6,pull_up=True)         # Rotary encoder pin B connected to GPIO6
+eventq = Queue.Queue(maxsize=10)
 
-def pin_a_rising():                    # Pin A event handler
-    if pin_b.is_pressed: print("-1")   # pin A rising while A is active is a clockwise turn
+enc_a = Button(5,pull_up=True)         # Rotary encoder pin A connected to GPIO5
+enc_b = Button(6,pull_up=True)         # Rotary encoder pin B connected to GPIO6
+enc_c = Button(26,pull_up=True)        # Rotary encoder push button connected to GPIO26
 
-def pin_b_rising():                    # Pin B event handler
-    if pin_a.is_pressed: print("1")    # pin B rising while A is active is a clockwise turn
+def enc_a_rising():                    # Pin A event handler
+    if enc_b.is_pressed: eventq.put(-1)   # pin A rising while B is active is a clockwise turn
 
-pin_a.when_pressed = pin_a_rising      # Register the event handler for pin A
-pin_b.when_pressed = pin_b_rising      # Register the event handler for pin B
+def enc_b_rising():                    # Pin B event handler
+    if enc_a.is_pressed: eventq.put(1)    # pin B rising while A is active is a clockwise turn
 
-input("Turn the knob, press Enter to quit.\n")
+def enc_c_rising():                    # Pin C event handler
+    eventq.put(2)    
+
+enc_a.when_pressed = enc_a_rising      # Register the event handler for pin A
+enc_b.when_pressed = enc_b_rising      # Register the event handler for pin B
+enc_c.when_pressed = enc_c_rising      # Register the event handler for pin C
+
+while True:
+    message = eventq.get()
+    print(message)
