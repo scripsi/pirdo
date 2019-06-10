b49c464cce81dc69e35290852f06961fd897028f
Scripsi <scripsi@scripsi.uk>
2019-06-10 11:35:05 +0100

    Added test for Sw1

--- encoder.py
+++ encoder.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python
 
-from gpiozero import Button
+import gpiozero
 import queue
 
 eventq = queue.Queue(maxsize=10)
 
-enc_a = Button(17,pull_up=True)         # Rotary encoder pin A connected to GPIO17
-enc_b = Button(27,pull_up=True)         # Rotary encoder pin B connected to GPIO27
-enc_c = Button(22,pull_up=True)         # Rotary encoder push button connected to GPIO22
+enc_a = gpiozero.Button(17,pull_up=True)         # Rotary encoder pin A connected to GPIO17
+enc_b = gpiozero.Button(27,pull_up=True)         # Rotary encoder pin B connected to GPIO27
+enc_c = gpiozero.Button(22,pull_up=True)         # Rotary encoder push button connected to GPIO22
+
+sw1_a = gpiozero.DigitalInputDevice(10,pull_up=True)
+sw1_b = gpiozero.DigitalInputDevice(9,pull_up=True)
+sw1_c = gpiozero.DigitalInputDevice(11,pull_up=True)
 
 def enc_a_rising():                    # Pin A event handler
     if enc_b.is_pressed: eventq.put(-1)   # pin A rising while B is active is a clockwise turn
@@ -15,12 +19,23 @@ def enc_a_rising():                    # Pin A event handler
 def enc_b_rising():                    # Pin B event handler
     if enc_a.is_pressed: eventq.put(1)    # pin B rising while A is active is a clockwise turn
 
-def enc_c_rising():                    # Pin C event handler
-    eventq.put(2)    
+def enc_c_released():                    # Pin C event handler
+    eventq.put(2)
+
+def sw1_changed():
+    eventq.put(3)
+
 
 enc_a.when_pressed = enc_a_rising      # Register the event handler for pin A
 enc_b.when_pressed = enc_b_rising      # Register the event handler for pin B
-enc_c.when_pressed = enc_c_rising      # Register the event handler for pin C
+enc_c.when_released = enc_c_released      # Register the event handler for pin C
+
+sw1_a.when_activated = sw1_changed
+sw1_a.when_deactivated = sw1_changed
+sw1_b.when_activated = sw1_changed
+sw1_b.when_deactivated = sw1_changed
+sw1_c.when_activated = sw1_changed
+sw1_c.when_deactivated = sw1_changed
 
 while True:
     message = eventq.get()
